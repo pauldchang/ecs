4fc4f781c3ba4bc75a87df302aab2ec9d87c87f2
pauldchang <153890789+pauldchang@users.noreply.github.com>
2024-05-29 19:16:20 -0500

    update

---
+++ ecs.tf
@@ -0,0 +1,41 @@
+resource "aws_ecs_cluster" "paul" {
+  name = "paul"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecs_task_definition" "service" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "pauldchang/stackdemo"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8000
+          hostPort      = 8000
+        }
+      ]
+    }
+  ])
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-east-2a, us-east-2b]"
+  }
+}
+
+resource "aws_s3_bucket" "paul-ecs" {
+
+}
